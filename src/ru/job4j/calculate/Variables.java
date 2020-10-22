e1460231652dba66ff05f3eb2196630583e4f0f7
kanadec-auk <kanadec-auk@rambler.ru>
2020-10-22 20:30:41 +0300

    0. Что такое переменная. [#380105]

---
+++ src/ru/job4j/calculate/Variables.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+            int age = 18;
+                System.out.println(age);
+            double length = 78.0332;
+                System.out.println(length);
+            boolean baby = true;
+                System.out.println(baby); }
+}
